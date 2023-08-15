d90eb7a12c49d828af1cdb010bd5da5d134083db
Obvious Plays <oz@abramovich.net>
2023-08-15 18:29:59 -0400

    Discorverd that not ui workes now

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,24 +3,50 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR;
 
-public class PlayerScript : MonoBehaviour
-{
+public class PlayerScript: MonoBehaviour {
+
     [SerializeField]
     private TMP_Text TimeText;
-
     public GameObject PauseUI;
+
+    private bool ActivePausetUI = false;
+
+    public InputActionReference toggleReference = null;
 
     void Update()
     {
         try
         {
-            string ActualTime = GameObject.Find("Payload").GetComponent<PayloadScript>().ActualTime;
+            string ActualTime = GameObject.Find("Payload").GetComponent < PayloadScript > ().ActualTime;
             TimeText.text = "Time: " + ActualTime;
         }
         catch
         {
             TimeText.text = "Not In Game";
+        }
+    }
+
+    private void Toggle(InputAction.CallbackContext context)
+    {
+        DisplayPauseMenu();
+    }
+
+    public void DisplayPauseMenu()
+    {
+        if (ActivePausetUI)
+        {
+            PauseUI.SetActive(false);
+            ActivePausetUI = false;
+            Time.timeScale = 1;
+        }
+        else if (!ActivePausetUI)
+        {
+            PauseUI.SetActive(true);
+            ActivePausetUI = true;
+            Time.timeScale = 0;
         }
     }
 }
